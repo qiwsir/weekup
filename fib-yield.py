3b557f581c689c06f36d640e349e0f428f1614eb
老齐 <qiwsir@gmail.com>
2020-11-02 14:26:06 +0800

    Update fib-yield.py

--- fib-yield.py
+++ fib-yield.py
@@ -13,7 +13,7 @@ def fib(max):
         yield b            #生成器函数，利用关键字yield一次性返回一个结果#
         a,b =b,a+b
         n = n+1
-    return 'done'          #给循环设置一个条件来退出循环，不然就会产生一个无限数列出来#
+    return 'done'          #给循环设置一个条件来退出循环，不然就会产生一个无限数列出来#   # 不需要这个，也不用设置max
 
 p = input("请输入1000以内的一个正整数k,程序将会输出斐波那契数列的前k项")
 while not p.isdigit():
